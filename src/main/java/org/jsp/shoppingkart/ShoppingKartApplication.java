3291825f06e4271683e2669fe53595afac91d2c9
saishkulkarni <saishkulkarni7@gmail.com>
2023-09-17 12:07:43 +0530

    Created Project

---
+++ src/main/java/org/jsp/shoppingkart/ShoppingKartApplication.java
@@ -0,0 +1,13 @@
+package org.jsp.shoppingkart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingKartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingKartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/jsp/shoppingkart/ShoppingKartApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jsp.shoppingkart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingKartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
